5f3fc007db1e8dddc563d0d9dc720c5784786fd0
Hayley Hartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2018-11-28 10:05:09 -0500

    updates

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # LIRI
 
-LIRI is an application made by me, Hayley Hartman, in October 2018. LIRI is a command line node app that returns information to the user based on the command entered. Users can use this app to search a specific movie, song, or musician, and the app returns information back through the console. Please see below for an example of how the concert-this functionality works, or reference the table below to see how other commands work.
+LIRI is an application made by me, Hayley Hartman, in October 2018. LIRI is a command line node app that returns information to the user based on the command entered. Users can use this app to search a specific movie, song, or musician, and the app returns information back through the console. 
+
+In order to run this app, clone this repository to your own computer, and enter the directory through terminal or bash. On entry, run npm install on the command line. Once you've done that, start each command with node liri and then the liri command. 
 
 Command | Source | Function
 ------- | ------ | -------- 
@@ -9,6 +11,9 @@ concert-this [+ a band name] | bands in town API | returns the next five shows t
 spotify-this-song [+ song title] | node spotify API | returns song information for a song of the user's choice
 do-this | fs and any of the above APIs | retrieves information from a separate document (formatted as command,thing to be searched)
 
-![image of concert-this](concert-this.png)
+### movie-this example
+![movie-this example](liri-movie.gif)
 
+### spotify-this-song example
+![spotify-this-song example](liri-song.gif)
 
--- liri.js
+++ liri.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
-var keys = require("./keys");
-var request = require('request');
-var fs = require("fs");
-var Spotify = require('node-spotify-api');
-var moment = require('moment')
+const keys = require("./keys");
+const request = require('request');
+const fs = require("fs");
+const Spotify = require('node-spotify-api');
+const moment = require('moment')
 
-var spotify = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
 
-var command = process.argv[2]
+let command = process.argv[2]
 
-var concertThis = function (input) {
-    var artist = input
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+concertThis = (input) => {
+    let artist = input
+    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
     if (!artist) {
         console.log("you didn't enter a band but you should go see Fleetwood Mac on October 26, 2018 in Cleveland.")
@@ -21,11 +21,11 @@ var concertThis = function (input) {
             console.log("error!")
             return
         }
-        var concertInfo = JSON.parse(body);
-        for (var i = 0; i < 5; i++) {
-            var artist = concertInfo[i].lineup
-            var venue = concertInfo[i].venue.name
-            var date = moment(concertInfo[i].datetime).format('MM/DD/YYYY')
+        let concertInfo = JSON.parse(body);
+        for (let i = 0; i < 5; i++) {
+            let artist = concertInfo[i].lineup
+            let venue = concertInfo[i].venue.name
+            let date = moment(concertInfo[i].datetime).format('MM/DD/YYYY')
             console.log(`Artist: ${artist}`)
             console.log(`Venue: ${venue}`)
             console.log(`Date: ${date}\n`)
@@ -33,8 +33,8 @@ var concertThis = function (input) {
     })
 }
 
-var songThis = function (input) {
-    var song = input
+songThis = (input) => {
+    let song = input
 
     spotify
         .search({
@@ -44,10 +44,10 @@ var songThis = function (input) {
         })
         .then(function (response) {
 
-            var artist = response.tracks.items[0].artists[0].name
-            var title = response.tracks.items[0].name
-            var previewUrl = response.tracks.items[0].preview_url
-            var album = response.tracks.items[0].album.name
+            let artist = response.tracks.items[0].artists[0].name
+            let title = response.tracks.items[0].name
+            let previewUrl = response.tracks.items[0].preview_url
+            let album = response.tracks.items[0].album.name
 
             console.log("Artist: " + artist)
             console.log("Title: " + title)
@@ -60,8 +60,8 @@ var songThis = function (input) {
         });
 }
 
-var movieThis = function (input) {
-    var movieQuery = ''
+movieThis = (input) => {
+    let movieQuery = ''
     if (!input) {
         title = "Mr. Nobody"
         movieQuery = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
@@ -76,7 +76,7 @@ var movieThis = function (input) {
             console.log("error!")
             return
         }
-        var movieInfo = JSON.parse(body);
+        let movieInfo = JSON.parse(body);
 
         console.log(`Title: ${movieInfo.Title}`)
         console.log(`Year: ${movieInfo.Year}`)
@@ -108,7 +108,7 @@ if (command === "concert-this") {
             return console.log(error);
         }
 
-        var dataArr = data.split(",");
+        let dataArr = data.split(",");
         command = dataArr[0]
 
         if (command === "concert-this") {
